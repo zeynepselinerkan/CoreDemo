3d1908719e1ab955650f1763abc6ff2b0476e9a3
zeynepselinerkan <Q2MCSUGt7ZjRYex>
2023-04-19 11:32:02 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Article.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Article
+    {
+        public int ArticleId { get; set; }
+        public string ArticleTitle { get; set; }
+        public string ArticleContent { get; set; }
+        public string ArticleThumbnailImage { get; set; } // Thumbnail Image - küçük kapak resmi
+        public string ArticleImage { get; set; }
+        public DateTime ArticleCreateDate { get; set; }
+        public bool ArticleStatus { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrete/Author.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        public string AuthorNameSurname { get; set; }
+        public string AuthorAbout { get; set; }
+        public string AuthorImage { get; set; }
+        public string AuthorMail { get; set; }
+        public string AuthorPassword { get; set; }
+        public bool AuthorStatus { get; set; }
+    }
+}
